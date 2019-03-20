d5f14aeab3c4e61ea357872e14d1489b635a2073
kushal atreya <kushalatreya@live.com>
2019-03-20 23:28:21 +0200

    main state established

--- new_form_app/src/App.js
+++ new_form_app/src/App.js
@@ -4,10 +4,26 @@ import PreviewComponent from "./components/PreviewComponent";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    AppStatePosts: [
+      {
+        Name: "kushal",
+        Ingridents: "Ingridents here",
+        Description: "Description here",
+        Type: "Type here",
+        Process: "Process here"
+      }
+    ]
+  };
+  addPost = obj => {
+    const tempPosts = this.state.AppStatePosts.slice();
+    tempPosts.push(obj);
+    this.setState({ AppStatePosts: tempPosts });
+  };
   render() {
     return (
       <div className="App">
-        <PreviewComponent />
+        <PreviewComponent addPost={this.addPost} />
       </div>
     );
   }
--- new_form_app/src/components/PreviewComponent.js
+++ new_form_app/src/components/PreviewComponent.js
@@ -1,75 +1,123 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function PreviewComponent() {
-  return (
-    <form className="m-5">
-      <div className="form-row">
-        <div className="form-group col-md-6">
-          <label for="Name">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="Name"
-            placeholder="Name of the dish"
-          />
-        </div>
-        <div className="form-group col-md-6">
-          <label for="Indrignts">Ingridents</label>
-          <input
-            type="text"
-            className="form-control"
-            id="Indrignts"
-            placeholder="tomatoes sauce, garlic paste"
-          />
-        </div>
-      </div>
+export default class PreviewComponent extends Component {
+  state = {
+    Name: "",
+    Ingridents: "",
+    Description: "",
+    Type: "",
+    Process: ""
+  };
+  saveSubmit = e => {
+    e.preventDefault();
+    this.props.addPost(this.state);
+  };
+  onNameChangeHandaler = e => {
+    this.setState({ Name: e.target.value });
+    console.log(e);
+  };
+  onIngridentsChangeHandaler = e => {
+    this.setState({ Ingridents: e.target.value });
+    console.log(e);
+  };
+  onDescriptionChangeHandaler = e => {
+    this.setState({
+      Description: e.target.value
+    });
+    console.log(e);
+  };
+  onTypeChangeHandaler = e => {
+    this.setState({ Type: e.target.value });
+    console.log(e);
+  };
+  onProcessChangeHandaler = e => {
+    this.setState({ Process: e.target.value });
+    console.log(e);
+  };
 
-      <div className="form-row">
-        <div className="form-group col-md-6">
-          <label for="inputCity">Description</label>
-          <input
+  render() {
+    return (
+      <form className="m-5" onSubmit={this.saveSubmit}>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label for="Name">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="Name"
+              placeholder="Name of the dish"
+              onChange={this.onNameChangeHandaler}
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label for="Indrignts">Ingridents</label>
+            <input
+              type="text"
+              className="form-control"
+              id="Indrignts"
+              placeholder="tomatoes sauce, garlic paste"
+              onChange={this.onIngridentsChangeHandaler}
+            />
+          </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label for="inputCity">Description</label>
+            <input
+              type="text"
+              className="form-control"
+              id="Desc"
+              placeholder="Description here "
+              onChange={this.onDescriptionChangeHandaler}
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label for="inputState">Type</label>
+            <select
+              id="inputState"
+              className="form-control"
+              onChange={this.onTypeChangeHandaler}
+            >
+              <option selected>Choose...</option>
+              <option>Breakfast</option>
+              <option>Lunch</option>
+              <option>Dinner</option>
+              <option>Supper</option>
+            </select>
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="Desc">Process</label>
+          <textarea
+            rows="4"
+            cols="50"
             type="text"
             className="form-control"
-            id="Desc"
-            placeholder="Description here "
+            id="Process"
+            placeholder="Please explain the process "
+            onChange={this.onProcessChangeHandaler}
           />
         </div>
-        <div className="form-group col-md-6">
-          <label for="inputState">Type</label>
-          <select id="inputState" className="form-control">
-            <option selected>Choose...</option>
-            <option>Breakfast</option>
-            <option>Lunch</option>
-            <option>Dinner</option>
-            <option>Supper</option>
-          </select>
+        <div className="form-group">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="gridCheck"
+            />
+            <label className="form-check-label" for="gridCheck">
+              Accept Terms and Condition
+            </label>
+          </div>
         </div>
-      </div>
-      <div className="form-group">
-        <label for="Desc">Process</label>
-        <textarea
-          rows="4"
-          cols="50"
-          type="text"
-          className="form-control"
-          id="Process"
-          placeholder="Please explain the process "
-        />
-      </div>
-      <div className="form-group">
-        <div className="form-check">
-          <input className="form-check-input" type="checkbox" id="gridCheck" />
-          <label className="form-check-label" for="gridCheck">
-            Accept Terms and Condition
-          </label>
-        </div>
-      </div>
-      <button type="submit" className="btn btn-outline-success">
-        Submit
-      </button>
-      <button type="submit" className="btn btn-outline-warning m-1">
-        Cancel
-      </button>
-    </form>
-  );
+        <button type="submit" className="btn btn-outline-success">
+          Submit
+        </button>
+        <button type="button" className="btn btn-outline-warning m-1">
+          Cancel
+        </button>
+      </form>
+    );
+  }
 }
